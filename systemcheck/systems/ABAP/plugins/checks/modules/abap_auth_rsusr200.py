4211f0f476dcc446dc0489389ca883af880b48ac
Lars Fasel <lars@team-fasel.com>
2017-07-26 15:50:04 -0500

    Additional SUIM plugins

--- systemcheck/systems/ABAP/plugins/checks/modules/abap_auth_rsusr200.py
+++ systemcheck/systems/ABAP/plugins/checks/modules/abap_auth_rsusr200.py
@@ -1,7 +1,7 @@
 import systemcheck.systems.ABAP.plugins as abapplugins
 
 
-class AbapCheck(abapplugins.CheckAbapRsusr002Plugin):
+class AbapCheck(abapplugins.CheckAbapRsusr200Plugin):
 
     def __init__(self):
         super().__init__()
